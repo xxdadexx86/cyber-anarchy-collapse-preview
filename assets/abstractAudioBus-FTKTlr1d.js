b4b04ba64e01a65c7fde7714602341d44b99884a
xxdadexx86 <dloddi86@gmail.com>
2026-07-29 23:41:44 +0200

    Publish ElevenLabs neural voice cast

---
+++ assets/abstractAudioBus-FTKTlr1d.js
@@ -0,0 +1,1 @@
+import{A as o}from"./webAudioBaseSubGraph-CnSf7bcU.js";class A extends o{constructor(s,t){super(s,t,3)}}export{A};
---
+++ assets/vertexColorMixing-DELngJx2.js
@@ -0,0 +1,224 @@
+import{S as e}from"./StrategicGlobeEngine-BfB_vdou.js";const t="bonesDeclaration",l=`#if NUM_BONE_INFLUENCERS>0
+#ifndef USE_VERTEX_PULLING
+attribute matricesIndices : vec4f;attribute matricesWeights : vec4f;
+#if NUM_BONE_INFLUENCERS>4
+attribute matricesIndicesExtra : vec4f;attribute matricesWeightsExtra : vec4f;
+#endif
+#endif
+#ifndef BAKED_VERTEX_ANIMATION_TEXTURE
+#ifdef BONETEXTURE
+var boneSampler : texture_2d<f32>;uniform boneTextureInfo : vec2f;
+#else
+uniform mBones : array<mat4x4f,BonesPerMesh>;
+#endif
+#ifdef BONES_VELOCITY_ENABLED
+uniform mPreviousBones : array<mat4x4f,BonesPerMesh>;
+#endif
+#ifdef BONETEXTURE
+fn readMatrixFromRawSampler(smp : texture_2d<f32>,index : f32)->mat4x4f
+{let offset=i32(index)*4; 
+let textureWidth=i32(uniforms.boneTextureInfo.x);let y=offset/textureWidth;let x=offset % textureWidth;let m0=textureLoad(smp,vec2<i32>(x+0,y),0);let m1=textureLoad(smp,vec2<i32>(x+1,y),0);let m2=textureLoad(smp,vec2<i32>(x+2,y),0);let m3=textureLoad(smp,vec2<i32>(x+3,y),0);return mat4x4f(m0,m1,m2,m3);}
+#endif
+#endif
+#endif
+`;e.IncludesShadersStoreWGSL[t]||(e.IncludesShadersStoreWGSL[t]=l);const V={name:t,shader:l},n="bakedVertexAnimationDeclaration",c=`#ifdef BAKED_VERTEX_ANIMATION_TEXTURE
+uniform bakedVertexAnimationTime: f32;uniform bakedVertexAnimationSettings: vec4<f32>;var bakedVertexAnimationTexture : texture_2d<f32>;
+#ifdef INSTANCES
+attribute bakedVertexAnimationSettingsInstanced : vec4<f32>;
+#endif
+fn readMatrixFromRawSamplerVAT(smp : texture_2d<f32>,index : f32,frame : f32)->mat4x4<f32>
+{let offset=i32(index)*4;let frameUV=i32(frame);let m0=textureLoad(smp,vec2<i32>(offset+0,frameUV),0);let m1=textureLoad(smp,vec2<i32>(offset+1,frameUV),0);let m2=textureLoad(smp,vec2<i32>(offset+2,frameUV),0);let m3=textureLoad(smp,vec2<i32>(offset+3,frameUV),0);return mat4x4<f32>(m0,m1,m2,m3);}
+#endif
+`;e.IncludesShadersStoreWGSL[n]||(e.IncludesShadersStoreWGSL[n]=c);const C={name:n,shader:c},r="instancesDeclaration",I=`#ifdef INSTANCES
+attribute world0 : vec4<f32>;attribute world1 : vec4<f32>;attribute world2 : vec4<f32>;attribute world3 : vec4<f32>;
+#ifdef INSTANCESCOLOR
+attribute instanceColor : vec4<f32>;
+#endif
+#if defined(THIN_INSTANCES) && !defined(WORLD_UBO)
+uniform world : mat4x4<f32>;
+#endif
+#if defined(VELOCITY) || defined(PREPASS_VELOCITY) || defined(PREPASS_VELOCITY_LINEAR) || defined(VELOCITY_LINEAR)
+attribute previousWorld0 : vec4<f32>;attribute previousWorld1 : vec4<f32>;attribute previousWorld2 : vec4<f32>;attribute previousWorld3 : vec4<f32>;
+#ifdef THIN_INSTANCES
+uniform previousWorld : mat4x4<f32>;
+#endif
+#endif
+#else
+#if !defined(WORLD_UBO)
+uniform world : mat4x4<f32>;
+#endif
+#if defined(VELOCITY) || defined(PREPASS_VELOCITY) || defined(PREPASS_VELOCITY_LINEAR) || defined(VELOCITY_LINEAR)
+uniform previousWorld : mat4x4<f32>;
+#endif
+#endif
+`;e.IncludesShadersStoreWGSL[r]||(e.IncludesShadersStoreWGSL[r]=I);const _={name:r,shader:I},i="clipPlaneVertexDeclaration",x=`#ifdef CLIPPLANE
+uniform vClipPlane: vec4<f32>;varying fClipDistance: f32;
+#endif
+#ifdef CLIPPLANE2
+uniform vClipPlane2: vec4<f32>;varying fClipDistance2: f32;
+#endif
+#ifdef CLIPPLANE3
+uniform vClipPlane3: vec4<f32>;varying fClipDistance3: f32;
+#endif
+#ifdef CLIPPLANE4
+uniform vClipPlane4: vec4<f32>;varying fClipDistance4: f32;
+#endif
+#ifdef CLIPPLANE5
+uniform vClipPlane5: vec4<f32>;varying fClipDistance5: f32;
+#endif
+#ifdef CLIPPLANE6
+uniform vClipPlane6: vec4<f32>;varying fClipDistance6: f32;
+#endif
+`;e.IncludesShadersStoreWGSL[i]||(e.IncludesShadersStoreWGSL[i]=x);const O={name:i,shader:x},a="fogVertexDeclaration",E=`#ifdef FOG
+varying vFogDistance: vec3f;
+#endif
+`;e.IncludesShadersStoreWGSL[a]||(e.IncludesShadersStoreWGSL[a]=E);const W={name:a,shader:E},f="instancesVertex",S=`#ifdef INSTANCES
+var finalWorld=mat4x4<f32>(vertexInputs.world0,vertexInputs.world1,vertexInputs.world2,vertexInputs.world3);
+#if defined(PREPASS_VELOCITY) || defined(VELOCITY) || defined(PREPASS_VELOCITY_LINEAR) || defined(VELOCITY_LINEAR)
+var finalPreviousWorld=mat4x4<f32>(
+vertexInputs.previousWorld0,vertexInputs.previousWorld1,
+vertexInputs.previousWorld2,vertexInputs.previousWorld3);
+#endif
+#ifdef THIN_INSTANCES
+#if !defined(WORLD_UBO)
+finalWorld=uniforms.world*finalWorld;
+#else
+finalWorld=mesh.world*finalWorld;
+#endif
+#if defined(PREPASS_VELOCITY) || defined(VELOCITY) || defined(PREPASS_VELOCITY_LINEAR) || defined(VELOCITY_LINEAR)
+finalPreviousWorld=uniforms.previousWorld*finalPreviousWorld;
+#endif
+#endif
+#else
+#if !defined(WORLD_UBO)
+var finalWorld=uniforms.world;
+#else
+var finalWorld=mesh.world;
+#endif
+#if defined(PREPASS_VELOCITY) || defined(VELOCITY) || defined(PREPASS_VELOCITY_LINEAR) || defined(VELOCITY_LINEAR)
+var finalPreviousWorld=uniforms.previousWorld;
+#endif
+#endif
+`;e.IncludesShadersStoreWGSL[f]||(e.IncludesShadersStoreWGSL[f]=S);const R={name:f,shader:S},s="bonesVertex",N=`#ifndef BAKED_VERTEX_ANIMATION_TEXTURE
+#if NUM_BONE_INFLUENCERS>0
+var influence : mat4x4<f32>;
+#ifdef BONETEXTURE
+influence=readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndices[0])*vertexInputs.matricesWeights[0];
+#if NUM_BONE_INFLUENCERS>1
+influence=influence+readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndices[1])*vertexInputs.matricesWeights[1];
+#endif 
+#if NUM_BONE_INFLUENCERS>2
+influence=influence+readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndices[2])*vertexInputs.matricesWeights[2];
+#endif 
+#if NUM_BONE_INFLUENCERS>3
+influence=influence+readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndices[3])*vertexInputs.matricesWeights[3];
+#endif 
+#if NUM_BONE_INFLUENCERS>4
+influence=influence+readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndicesExtra[0])*vertexInputs.matricesWeightsExtra[0];
+#endif 
+#if NUM_BONE_INFLUENCERS>5
+influence=influence+readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndicesExtra[1])*vertexInputs.matricesWeightsExtra[1];
+#endif 
+#if NUM_BONE_INFLUENCERS>6
+influence=influence+readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndicesExtra[2])*vertexInputs.matricesWeightsExtra[2];
+#endif 
+#if NUM_BONE_INFLUENCERS>7
+influence=influence+readMatrixFromRawSampler(boneSampler,vertexInputs.matricesIndicesExtra[3])*vertexInputs.matricesWeightsExtra[3];
+#endif 
+#else 
+influence=uniforms.mBones[i32(vertexInputs.matricesIndices[0])]*vertexInputs.matricesWeights[0];
+#if NUM_BONE_INFLUENCERS>1
+influence=influence+uniforms.mBones[i32(vertexInputs.matricesIndices[1])]*vertexInputs.matricesWeights[1];
+#endif 
+#if NUM_BONE_INFLUENCERS>2
+influence=influence+uniforms.mBones[i32(vertexInputs.matricesIndices[2])]*vertexInputs.matricesWeights[2];
+#endif 
+#if NUM_BONE_INFLUENCERS>3
+influence=influence+uniforms.mBones[i32(vertexInputs.matricesIndices[3])]*vertexInputs.matricesWeights[3];
+#endif 
+#if NUM_BONE_INFLUENCERS>4
+influence=influence+uniforms.mBones[i32(vertexInputs.matricesIndicesExtra[0])]*vertexInputs.matricesWeightsExtra[0];
+#endif 
+#if NUM_BONE_INFLUENCERS>5
+influence=influence+uniforms.mBones[i32(vertexInputs.matricesIndicesExtra[1])]*vertexInputs.matricesWeightsExtra[1];
+#endif 
+#if NUM_BONE_INFLUENCERS>6
+influence=influence+uniforms.mBones[i32(vertexInputs.matricesIndicesExtra[2])]*vertexInputs.matricesWeightsExtra[2];
+#endif 
+#if NUM_BONE_INFLUENCERS>7
+influence=influence+uniforms.mBones[i32(vertexInputs.matricesIndicesExtra[3])]*vertexInputs.matricesWeightsExtra[3];
+#endif 
+#endif
+finalWorld=finalWorld*influence;
+#endif
+#endif
+`;e.IncludesShadersStoreWGSL[s]||(e.IncludesShadersStoreWGSL[s]=N);const h={name:s,shader:N},d="bakedVertexAnimation",v=`#ifdef BAKED_VERTEX_ANIMATION_TEXTURE
+{
+#ifdef INSTANCES
+let VATStartFrame: f32=vertexInputs.bakedVertexAnimationSettingsInstanced.x;let VATEndFrame: f32=vertexInputs.bakedVertexAnimationSettingsInstanced.y;let VATOffsetFrame: f32=vertexInputs.bakedVertexAnimationSettingsInstanced.z;let VATSpeed: f32=vertexInputs.bakedVertexAnimationSettingsInstanced.w;
+#else
+let VATStartFrame: f32=uniforms.bakedVertexAnimationSettings.x;let VATEndFrame: f32=uniforms.bakedVertexAnimationSettings.y;let VATOffsetFrame: f32=uniforms.bakedVertexAnimationSettings.z;let VATSpeed: f32=uniforms.bakedVertexAnimationSettings.w;
+#endif
+let totalFrames: f32=VATEndFrame-VATStartFrame+1.0;let time: f32=uniforms.bakedVertexAnimationTime*VATSpeed/totalFrames;let frameCorrection: f32=select(1.0,0.0,time<1.0);let numOfFrames: f32=totalFrames-frameCorrection;var VATFrameNum: f32=fract(time)*numOfFrames;VATFrameNum=(VATFrameNum+VATOffsetFrame) % numOfFrames;VATFrameNum=floor(VATFrameNum);VATFrameNum=VATFrameNum+VATStartFrame+frameCorrection;var VATInfluence : mat4x4<f32>;VATInfluence=readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndices[0],VATFrameNum)*vertexInputs.matricesWeights[0];
+#if NUM_BONE_INFLUENCERS>1
+VATInfluence=VATInfluence+readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndices[1],VATFrameNum)*vertexInputs.matricesWeights[1];
+#endif
+#if NUM_BONE_INFLUENCERS>2
+VATInfluence=VATInfluence+readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndices[2],VATFrameNum)*vertexInputs.matricesWeights[2];
+#endif
+#if NUM_BONE_INFLUENCERS>3
+VATInfluence=VATInfluence+readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndices[3],VATFrameNum)*vertexInputs.matricesWeights[3];
+#endif
+#if NUM_BONE_INFLUENCERS>4
+VATInfluence=VATInfluence+readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndicesExtra[0],VATFrameNum)*vertexInputs.matricesWeightsExtra[0];
+#endif
+#if NUM_BONE_INFLUENCERS>5
+VATInfluence=VATInfluence+readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndicesExtra[1],VATFrameNum)*vertexInputs.matricesWeightsExtra[1];
+#endif
+#if NUM_BONE_INFLUENCERS>6
+VATInfluence=VATInfluence+readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndicesExtra[2],VATFrameNum)*vertexInputs.matricesWeightsExtra[2];
+#endif
+#if NUM_BONE_INFLUENCERS>7
+VATInfluence=VATInfluence+readMatrixFromRawSamplerVAT(bakedVertexAnimationTexture,vertexInputs.matricesIndicesExtra[3],VATFrameNum)*vertexInputs.matricesWeightsExtra[3];
+#endif
+finalWorld=finalWorld*VATInfluence;}
+#endif
+`;e.IncludesShadersStoreWGSL[d]||(e.IncludesShadersStoreWGSL[d]=v);const F={name:d,shader:v},o="clipPlaneVertex",p=`#ifdef CLIPPLANE
+vertexOutputs.fClipDistance=dot(worldPos,uniforms.vClipPlane);
+#endif
+#ifdef CLIPPLANE2
+vertexOutputs.fClipDistance2=dot(worldPos,uniforms.vClipPlane2);
+#endif
+#ifdef CLIPPLANE3
+vertexOutputs.fClipDistance3=dot(worldPos,uniforms.vClipPlane3);
+#endif
+#ifdef CLIPPLANE4
+vertexOutputs.fClipDistance4=dot(worldPos,uniforms.vClipPlane4);
+#endif
+#ifdef CLIPPLANE5
+vertexOutputs.fClipDistance5=dot(worldPos,uniforms.vClipPlane5);
+#endif
+#ifdef CLIPPLANE6
+vertexOutputs.fClipDistance6=dot(worldPos,uniforms.vClipPlane6);
+#endif
+`;e.IncludesShadersStoreWGSL[o]||(e.IncludesShadersStoreWGSL[o]=p);const P={name:o,shader:p},m="fogVertex",A=`#ifdef FOG
+#ifdef SCENE_UBO
+vertexOutputs.vFogDistance=(scene.view*worldPos).xyz;
+#else
+vertexOutputs.vFogDistance=(uniforms.view*worldPos).xyz;
+#endif
+#endif
+`;e.IncludesShadersStoreWGSL[m]||(e.IncludesShadersStoreWGSL[m]=A);const U={name:m,shader:A},u="vertexColorMixing",L=`#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+vertexOutputs.vColor=vec4f(1.0);
+#ifdef VERTEXCOLOR
+#ifdef VERTEXALPHA
+vertexOutputs.vColor*=colorUpdated;
+#else
+vertexOutputs.vColor=vec4f(vertexOutputs.vColor.rgb*colorUpdated.rgb,vertexOutputs.vColor.a);
+#endif
+#endif
+#ifdef INSTANCESCOLOR
+vertexOutputs.vColor*=vertexInputs.instanceColor;
+#endif
+#endif
+`;e.IncludesShadersStoreWGSL[u]||(e.IncludesShadersStoreWGSL[u]=L);const b={name:u,shader:L};export{C as a,V as b,O as c,R as d,h as e,W as f,F as g,P as h,_ as i,U as j,b as v};
