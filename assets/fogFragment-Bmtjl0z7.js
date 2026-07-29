9de08837d22f0683f5ab64b71f90103f4d7a848a
xxdadexx86 <dloddi86@gmail.com>
2026-07-29 22:09:28 +0200

    Publish mobile globe and guided tutorial fixes

---
+++ assets/fogFragment-Bmtjl0z7.js
@@ -0,0 +1,68 @@
+import{S as e}from"./StrategicGlobeEngine-BB690pry.js";const f="clipPlaneFragmentDeclaration",a=`#ifdef CLIPPLANE
+varying float fClipDistance;
+#endif
+#ifdef CLIPPLANE2
+varying float fClipDistance2;
+#endif
+#ifdef CLIPPLANE3
+varying float fClipDistance3;
+#endif
+#ifdef CLIPPLANE4
+varying float fClipDistance4;
+#endif
+#ifdef CLIPPLANE5
+varying float fClipDistance5;
+#endif
+#ifdef CLIPPLANE6
+varying float fClipDistance6;
+#endif
+`;e.IncludesShadersStore[f]||(e.IncludesShadersStore[f]=a);const c={name:f,shader:a},i="fogFragmentDeclaration",d=`#ifdef FOG
+#define FOGMODE_NONE 0.
+#define FOGMODE_EXP 1.
+#define FOGMODE_EXP2 2.
+#define FOGMODE_LINEAR 3.
+#define E 2.71828
+uniform vec4 vFogInfos;uniform vec3 vFogColor;varying vec3 vFogDistance;float CalcFogFactor()
+{float fogCoeff=1.0;float fogStart=vFogInfos.y;float fogEnd=vFogInfos.z;float fogDensity=vFogInfos.w;float fogDistance=length(vFogDistance);if (FOGMODE_LINEAR==vFogInfos.x)
+{fogCoeff=(fogEnd-fogDistance)/(fogEnd-fogStart);}
+else if (FOGMODE_EXP==vFogInfos.x)
+{fogCoeff=1.0/pow(E,fogDistance*fogDensity);}
+else if (FOGMODE_EXP2==vFogInfos.x)
+{fogCoeff=1.0/pow(E,fogDistance*fogDistance*fogDensity*fogDensity);}
+return clamp(fogCoeff,0.0,1.0);}
+#endif
+`;e.IncludesShadersStore[i]||(e.IncludesShadersStore[i]=d);const l={name:i,shader:d},n="clipPlaneFragment",s=`#if defined(CLIPPLANE) || defined(CLIPPLANE2) || defined(CLIPPLANE3) || defined(CLIPPLANE4) || defined(CLIPPLANE5) || defined(CLIPPLANE6)
+if (false) {}
+#endif
+#ifdef CLIPPLANE
+else if (fClipDistance>0.0)
+{discard;}
+#endif
+#ifdef CLIPPLANE2
+else if (fClipDistance2>0.0)
+{discard;}
+#endif
+#ifdef CLIPPLANE3
+else if (fClipDistance3>0.0)
+{discard;}
+#endif
+#ifdef CLIPPLANE4
+else if (fClipDistance4>0.0)
+{discard;}
+#endif
+#ifdef CLIPPLANE5
+else if (fClipDistance5>0.0)
+{discard;}
+#endif
+#ifdef CLIPPLANE6
+else if (fClipDistance6>0.0)
+{discard;}
+#endif
+`;e.IncludesShadersStore[n]||(e.IncludesShadersStore[n]=s);const g={name:n,shader:s},o="fogFragment",t=`#ifdef FOG
+float fog=CalcFogFactor();
+#ifdef PBR
+fog=toLinearSpace(fog);
+#endif
+color.rgb=mix(vFogColor,color.rgb,fog);
+#endif
+`;e.IncludesShadersStore[o]||(e.IncludesShadersStore[o]=t);const P={name:o,shader:t};export{g as a,P as b,c,l as f};
