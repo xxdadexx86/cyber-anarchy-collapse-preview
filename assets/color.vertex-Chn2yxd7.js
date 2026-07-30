1fb96adb6fc540e7ddcb47635a1a0198dfcbfb5d
xxdadexx86 <dloddi86@gmail.com>
2026-07-30 07:12:02 +0200

    Deploy corrected political globe

---
+++ assets/color.vertex-Chn2yxd7.js
@@ -0,0 +1,39 @@
+import{S as e}from"./StrategicGlobeEngine-XS756Zd2.js";import{b as r,a as t,c as a,f as d,i as c,d as l,e as s,g as f,h as V,j as x,v as S}from"./vertexColorMixing-CCm2hL2T.js";import"./index-C7CVCWpc.js";const o="colorVertexShader",n=`attribute vec3 position;
+#ifdef VERTEXCOLOR
+attribute vec4 color;
+#endif
+#include<bonesDeclaration>
+#include<bakedVertexAnimationDeclaration>
+#include<clipPlaneVertexDeclaration>
+#include<fogVertexDeclaration>
+#ifdef FOG
+uniform mat4 view;
+#endif
+#include<instancesDeclaration>
+uniform mat4 viewProjection;
+#ifdef MULTIVIEW
+uniform mat4 viewProjectionR;
+#endif
+#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+varying vec4 vColor;
+#endif
+#define CUSTOM_VERTEX_DEFINITIONS
+void main(void) {
+#define CUSTOM_VERTEX_MAIN_BEGIN
+#ifdef VERTEXCOLOR
+vec4 colorUpdated=color;
+#endif
+#include<instancesVertex>
+#include<bonesVertex>
+#include<bakedVertexAnimation>
+vec4 worldPos=finalWorld*vec4(position,1.0);
+#ifdef MULTIVIEW
+if (gl_ViewID_OVR==0u) {gl_Position=viewProjection*worldPos;} else {gl_Position=viewProjectionR*worldPos;}
+#else
+gl_Position=viewProjection*worldPos;
+#endif
+#include<clipPlaneVertex>
+#include<fogVertex>
+#include<vertexColorMixing>
+#define CUSTOM_VERTEX_MAIN_END
+}`;e.ShadersStore[o]||(e.ShadersStore[o]=n);const u=[r,t,a,d,c,l,s,f,V,x,S];for(const i of u)e.IncludesShadersStore[i.name]||(e.IncludesShadersStore[i.name]=i.shader);const P={name:o,shader:n};export{P as colorVertexShader};
---
+++ assets/rgbdDecode.fragment-DmGMwxPP.js
@@ -0,0 +1,5 @@
+import{S as e}from"./StrategicGlobeEngine-XS756Zd2.js";import{h as n}from"./helperFunctions-DrRvnhH5.js";import"./index-C7CVCWpc.js";const o="rgbdDecodePixelShader",t=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=t);const a=[n];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const S={name:o,shader:t};export{S as rgbdDecodePixelShader};
